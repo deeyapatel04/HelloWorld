1f48f3c48f5092a8d8ddf6b2dfd6e2b0545a3ac2
Deeya <deeyapatel04@gmail.com>
2021-08-14 22:59:21 -0500

    first git commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,33 @@
+package com.company;
+import java.io.*;
+
+
+public class Main {
+
+    public static void main(String[] args) {
+        int x = 10;
+        int y = 10;
+	    // write your code here
+        System.out.println("Hello World");
+        System.out.println(x*x);
+        System.out.println(y);
+
+        System.out.println(add(x, y));
+        System.out.println(add(10, 20));
+        System.out.println(add(x, y));
+        System.out.println(add(x, y));
+
+        int total = 0;
+        for (int i=0; i<5; i=i+1) {
+            total = total + i;
+            System.out.println("loop-start");
+            System.out.format("i=%d, total=%d\n", i, total);
+            System.out.println("loop-end");
+        }
+        //System.out.println(total);
+    }
+
+    public static int add(int x, int y) {
+        return x+y;
+    }
+}
